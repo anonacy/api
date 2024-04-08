478da5e87366839a64d96271e9d9d5789f5a74c5
hewham <hamiltonhew@gmail.com>
2024-04-07 23:14:56 -0400

    more docs cleanup

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -8,26 +8,24 @@ const api = {
     info: {
       title: '@anonacy/postal-api',
       version,
-      description: `<b>- About</b><br/><br/>Anonacy's postal api enables control over a [Postal](https://postalserver.io) mail server. It is designed for email aliasing, by managing the 'Routing' system within Postal, while Postal handles the smtp forwarding.
+      description: `<b>- About</b><br/><br/>Anonacy's postal api enables control over a smtp mail server. It is designed for email aliasing, by managing the 'Routing' system within [Postal](https://postalserver.io), which handles the smtp forwarding.
       <br/><br/>
-      The API can be used in many ways, and will continually be enhanced. It works by utilizing direct mySQL queries in Postal's database, as well as using [puppeteer](https://pptr.dev) for more complex interactions.
+      This system is used to power the [anonacy](https://anonacy.com) and [hiddenlogin](https://hiddenlogin.com) platforms.
       <br/><br/>
-      This system is used to power the [anonacy](anonacy.com) and [hiddenlogin](https://hiddenlogin.com) platforms.
-      <br/><br/>
-      It is mounted as the [v2] api at [api.anonacy.com/v2](https://api.anonacy.com/v2/docs). Currently, the [v1](https://api.anonacy.com/v1/docs) api has been updated with logic to use the v2 api for the relevant alias management to preserve functionality of the current platform.
+      It is mounted as the [v2] api at [api.anonacy.com/v2](https://api.anonacy.com/v2/docs/). Currently, the [v1](https://api.anonacy.com/v1/docs) api has been updated with logic to use the v2 api for the relevant alias management to preserve functionality of the current platform.
       <br/><hr/>
       <b>- Authorization</b>
       <br/><br/>
       All requests require the header: '<b>Authorization: Bearer APIKEY</b>'
       <br/><br/>
-      This api uses an api key with [Bearer](https://swagger.io/docs/specification/authentication/bearer-authentication/) authorization. The api key will be provided to you or it can be generated using the credentials system in the Postal server. The api key will automatically scope all requests to the Postal organization & server that it is assigned for.
+      This api uses an api key with [Bearer](https://swagger.io/docs/specification/authentication/bearer-authentication/) authorization. The api key will be provided to you or it can be generated using the credentials system in the Postal server. The api key will automatically scope all requests to the organization & server that it is assigned for.
       <br/><br/>
       The api key needs to be attached to ALL requests using the '<b>Authorization</b>' header, with the value set as '<b>Bearer APIKEY</b>'. Always use https when making requests to the api.
       <br/><br/><hr/>
       <b>- General Usage</b>
       <br/><br/>
       To get started, you should first add and setup a domain name, then you can add aliases.
-      <ul><li>[POST] /domain - add a domain</li><li>[GET] /domain?domain=[domain_name] - domain dns setup info</li><li>[POST] /alias - Create alias</li><li>[PUT] /alias - toggle alias forwarding</li></ul>
+      <ul><li>[POST] /domain - add a domain</li><li>[GET] /domain - domain dns setup info</li><li>[POST] /alias - Create alias</li></ul>
       <br/>
       Full api functionality is found below, along with request and response data. The api endpoints are mounted at /v2. For example, [/domains] in these docs maps to [api.anonacy.com/v2/domains].
       <br/><br/><hr/>
